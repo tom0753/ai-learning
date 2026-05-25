1896fbc1a7bf7b3ef74a8ec664aed2c69644f64c
Tomasz <chodtom@o2.pl>
2026-05-25 18:39:28 +0200

    small program that generates squares something like in mario

---
+++ week2/mario.py
@@ -0,0 +1,11 @@
+def main():
+    print_square(3)
+
+
+def print_square(size):
+    for i in range(size):
+        # print("#" * size)
+        for j in range(size):
+            print("#", end="")
+        print()
+main()
