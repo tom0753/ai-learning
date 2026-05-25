2732dedde1a9c8788007b54aa9b43fca7198338f
Tomasz <chodtom@o2.pl>
2026-05-25 15:54:11 +0200

    more fun with loops and functions

---
+++ week2/cats.py
@@ -0,0 +1,17 @@
+def main():
+    times = get_number()
+    meow(times)
+
+def get_number():
+    while True:
+        n = int(input("Ile wynosi n? "))
+        if n > 0:
+            return n
+
+
+def meow(n):
+    for i in range(n):
+        print("meow")
+
+
+main()
