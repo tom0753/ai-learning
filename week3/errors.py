4ef9436dc8983bf7b08909af827e5aafaac5016e
Tomasz <chodtom@o2.pl>
2026-05-30 22:31:28 +0200

    more fun with errors

--- week3/errors.py
+++ week3/errors.py
@@ -1,6 +1,8 @@
-try: 
-    x = int(input("Podaj wartosc x: "))
-except ValueError:
-    print("x nie jest integerem!")
-else:
-    print(f"x wynosi {x}.")
+while True:
+    try: 
+        x = int(input("Podaj wartosc x: "))
+    except ValueError:
+        print("x nie jest integerem!")
+    else:
+        break
+print(f"x wynosi {x}")
